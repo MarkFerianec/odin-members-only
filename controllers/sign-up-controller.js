baea915233d7fbb181903a3b325ee8497718ecb0
Mark Ferianec <mjf397@gmail.com>
2024-11-29 21:04:10 -0700

    Implement validation and sanitization in form

--- controllers/sign-up-controller.js
+++ controllers/sign-up-controller.js
@@ -1,4 +1,33 @@
+const { body, validationResult } = require("express-validator");
+
 exports.getSignUpForm = (req, res, next) => {
-    res.render("sign-up");
-  };
-  
+  res.render("sign-up");
+};
+
+const validatePassword = [
+  body("confirmPassword")
+    .trim()
+    .isLength({ min: 4, max: 16 })
+    .withMessage("Password must be between 4 to 16 characters")
+    .custom(async (confirmPassword, { req }) => {
+      const password = req.body.password;
+
+      if (password !== confirmPassword) {
+        throw new Error("Passwords must be same");
+      }
+    })
+    .escape(),
+];
+
+exports.postSignUpForm = [
+  validatePassword,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      res.send("No errors");
+    } else {
+      res.render("sign-up", { errors: errors.array() });
+    }
+  },
+];
--- routes/router.js
+++ routes/router.js
@@ -8,5 +8,6 @@ const signUpController = require("../controllers/sign-up-controller");
 router.get("/", indexController.getIndex);
 
 router.get("/sign-up", signUpController.getSignUpForm);
+router.post("/sign-up", signUpController.postSignUpForm);
 
 module.exports = router;
