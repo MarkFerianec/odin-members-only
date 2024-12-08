f2f72e7cda581dd66cb17fce879878dcd22ab0bc
Mark Ferianec <mjf397@gmail.com>
2024-12-07 20:33:34 -0700

    Add navigation links to create message page

--- controllers/create-message-controller.js
+++ controllers/create-message-controller.js
@@ -1,3 +1,15 @@
+const links = require("../utils/links");
+
 exports.getCreateMessage = (req, res, next) => {
-  res.render("create-message");
+  if (req.isAuthenticated() && req.user.membership_status) {
+    res.render("create-message", {
+      links: links.userAndClubMemberLinks,
+    });
+  } else if (req.isAuthenticated()) {
+    res.render("create-message", {
+      links: links.userNonClubMemberLinks,
+    });
+  } else {
+    res.send("Please sign up and/or log in first");
+  }
 };
