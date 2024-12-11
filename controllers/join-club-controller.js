07052c114231d754811c73191218ea6d3a63ea7a
Mark Ferianec <mjf397@gmail.com>
2024-12-10 18:22:21 -0700

    Add validation for becoming club member

--- controllers/join-club-controller.js
+++ controllers/join-club-controller.js
@@ -2,6 +2,8 @@ const links = require("../utils/links");
 
 const db = require("../db/queries");
 
+const { body, validationResult } = require("express-validator");
+
 exports.getJoinClub = (req, res, next) => {
   if (
     req.isAuthenticated() &&
@@ -29,11 +31,27 @@ exports.getJoinClub = (req, res, next) => {
   }
 };
 
-exports.postJoinClub = async (req, res, next) => {
-  if (req.body.passcode === "1234") {
-    await db.becomeClubMember(req.user.username);
-    res.send("Correct passcode");
-  } else {
-    res.send("Wrong passcode");
-  }
-};
+const validatePasscode = [
+  body("passcode")
+    .trim()
+    .isLength({ min: 4, max: 12 })
+    .withMessage("Passcode must be between 4 and 12 characters")
+    .escape(),
+];
+
+exports.postJoinClub = [
+  validatePasscode,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty() && req.body.passcode === "1234") {
+      await db.becomeClubMember(req.user.username);
+      res.redirect("/");
+    } else {
+      res.render("join-club", {
+        errors: errors.array(),
+        links: links.userNonClubMemberLinks,
+      });
+    }
+  },
+];
