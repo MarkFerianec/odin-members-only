534d5cb5cac7f413fb16468ac26c314f5db280fa
Mark Ferianec <mjf397@gmail.com>
2024-12-10 18:06:02 -0700

    Add validation for creating messages

--- controllers/create-message-controller.js
+++ controllers/create-message-controller.js
@@ -2,6 +2,8 @@ const links = require("../utils/links");
 
 const db = require("../db/queries");
 
+const { body, validationResult } = require("express-validator");
+
 exports.getCreateMessage = (req, res, next) => {
   if (
     req.isAuthenticated() &&
@@ -10,7 +12,6 @@ exports.getCreateMessage = (req, res, next) => {
   ) {
     res.render("create-message", {
       links: links.clubMemberAndAdminLinks,
-      user: req.user,
     });
   } else if (req.isAuthenticated() && req.user.membership_status) {
     res.render("create-message", {
@@ -25,15 +26,60 @@ exports.getCreateMessage = (req, res, next) => {
   }
 };
 
-exports.postCreateMessage = async (req, res, next) => {
-  const timestamp = new Date().toString();
+const validateTitle = [
+  body("title")
+    .trim()
+    .isLength({ min: 4, max: 25 })
+    .withMessage("Title must be between 4 and 25 characters")
+    .escape(),
+];
 
-  await db.createMessage(
-    req.user.username,
-    req.body.title,
-    req.body.text,
-    timestamp
-  );
+const validateText = [
+  body("text")
+    .trim()
+    .isLength({ min: 4, max: 50 })
+    .withMessage("Text must be between 4 and 50 characters")
+    .escape(),
+];
 
-  res.redirect("/");
-};
+exports.postCreateMessage = [
+  validateTitle,
+  validateText,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      const timestamp = new Date().toString();
+
+      await db.createMessage(
+        req.user.username,
+        req.body.title,
+        req.body.text,
+        timestamp
+      );
+
+      res.redirect("/");
+    } else if (
+      req.isAuthenticated() &&
+      req.user.membership_status &&
+      req.user.admin_status
+    ) {
+      res.render("create-message", {
+        links: links.clubMemberAndAdminLinks,
+        errors: errors.array(),
+      });
+    } else if (req.isAuthenticated() && req.user.membership_status) {
+      res.render("create-message", {
+        links: links.userAndClubMemberLinks,
+        errors: errors.array(),
+      });
+    } else if (req.isAuthenticated()) {
+      res.render("create-message", {
+        links: links.userNonClubMemberLinks,
+        errors: errors.array(),
+      });
+    } else {
+      res.send("Please sign up and/or log in first");
+    }
+  },
+];
